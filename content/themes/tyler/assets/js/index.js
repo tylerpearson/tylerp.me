01f98654ddbb82a85d3e22a456b9ed1709f9f1b4
Tyler Pearson <ty.pearson@gmail.com>
2015-07-17 23:12:10 -0700

    Adjust displaying Dribbble on template

--- content/themes/tyler/assets/js/index.js
+++ content/themes/tyler/assets/js/index.js
@@ -1,21 +1,22 @@
 (function() {
 
-    var DRIBBBLE_API_TOKEN = '2982b6f5520299137430aac46d2532cc3b00583ff8c39378471b5e5cf6117c61';
+    if ($('body').hasClass('home-template')) {
 
-    $.get('https://api.dribbble.com/v1/users/tylerpearson/shots?access_token=' + DRIBBBLE_API_TOKEN)
-        .then(function(shots) {
-            var html = [];
+        var DRIBBBLE_API_TOKEN = '2982b6f5520299137430aac46d2532cc3b00583ff8c39378471b5e5cf6117c61';
 
-            for (var i=0; i<10; i++) {
-                var shot = shots[i];
-                html.push('<li class="shot-wrap" style="background-image: url(' + shot.images.normal + ');">');
-                html.push('<a href="' + shot.html_url + '">' + shot.title + '</a>');
-                html.push('</li>');
-            }
+        $.get('https://api.dribbble.com/v1/users/tylerpearson/shots?access_token=' + DRIBBBLE_API_TOKEN)
+            .then(function(shots) {
+                var html = [];
 
-            console.log(shots.length);
+                for (var i=0; i<10; i++) {
+                    var shot = shots[i];
+                    html.push('<li class="shot-wrap" style="background-image: url(' + shot.images.normal + ');">');
+                    html.push('<a href="' + shot.html_url + '">' + shot.title + '</a>');
+                    html.push('</li>');
+                }
 
-            $('.js-dribbble-list').html(html.join(''));
-        });
+                $('.js-dribbble-list').html(html.join(''));
+            });
+    }
 
 }());
